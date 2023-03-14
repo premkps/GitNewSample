3ceefd766f355b13d9181b45c2eed5f879a5e6ec
premkps <premkumarkps110@gmail.com>
2023-03-14 14:51:28 +0530

    Just project push

---
+++ src/org/test/NewSample.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class NewSample {
+	public static void main(String[] args) {
+		System.out.println(2);
+		System.out.println(2);
+		System.out.println(2);
+		System.out.println(2);
+		System.out.println(2);
+		
+	}
+
+}
